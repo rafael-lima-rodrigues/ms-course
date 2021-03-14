8aaa43f0548cdeddacf244013a257e744ca4adee
Rafael <rafaa.verdao@gmail.com>
2021-03-14 18:31:32 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/main/java/com/rafarodrigues/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.rafarodrigues.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+}
