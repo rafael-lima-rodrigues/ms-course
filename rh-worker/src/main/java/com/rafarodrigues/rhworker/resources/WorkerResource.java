610f97b82bf50f68a2fd80e464457ba53a352ae7
Rafael <rafaa.verdao@gmail.com>
2021-03-21 15:03:52 -0300

    db config server

--- rh-worker/src/main/java/com/rafarodrigues/rhworker/resources/WorkerResource.java
+++ rh-worker/src/main/java/com/rafarodrigues/rhworker/resources/WorkerResource.java
@@ -24,8 +24,6 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
 
     @Autowired
     private Environment env;
@@ -35,7 +33,6 @@ public class WorkerResource {
 
     @GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
-        logger.info("CONFIG = " + testConfig);
         return ResponseEntity.noContent().build();
     }
 
